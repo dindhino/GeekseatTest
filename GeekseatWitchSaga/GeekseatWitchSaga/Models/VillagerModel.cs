068198523fd46409c5e4164980bb0a40282e8166
Dindin Dhino Alamsyah <bdi.developer@bosnetdis.com>
2022-08-31 04:14:27 +0700

    Add Model

---
+++ GeekseatWitchSaga/GeekseatWitchSaga/Models/VillagerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekseatWitchSaga.Models
+{
+    public class VillagerData
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed.")]
+        public int iAge { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed.")]
+        public int iYear { get; set; }
+    }
+}
